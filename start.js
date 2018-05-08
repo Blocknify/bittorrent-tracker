68d194493a726e629e4ed802d0513db0565710ff
Rinor Bytyci <rinorb@gmail.com>
2018-05-08 16:19:46 +0200

    Add Start Script

--- start.js
+++ start.js
@@ -50,7 +50,7 @@ server.on('listening', function () {
 })
 
 // start tracker server listening! Use 0 to listen on a random free port.
-server.listen(port, hostname, onlistening)
+server.listen(1900, "localhost")
 
 // listen for individual tracker messages from peers:
 
